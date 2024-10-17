12087cbee17574e03c5688383aebaa67d0e5cbee
Shivam Jadon <shivamjadon5847@gmail.com>
2024-10-17 19:04:02 +0530

    done

--- src/main/java/com/example/bookmyshow/EventConsumer/ReverseBooking.java
+++ src/main/java/com/example/bookmyshow/EventConsumer/ReverseBooking.java
@@ -10,37 +10,37 @@ import java.util.List;
 
 
 public class ReverseBooking {
-//     private final BookingRepository bookingRepository;
-//     private final ShowSeatRepository showSeatRepository;
-//     public ReverseBooking(BookingRepository bookingRepository, ShowSeatRepository showSeatRepository) {
-//         this.bookingRepository = bookingRepository;
-//         this.showSeatRepository = showSeatRepository;
-//     }
-//
-//     @KafkaListener(topics="reverse-booking",groupId = "booking-group")
-//     public void reverseBooking(BookingEvent bookingEvent){
-//          System.out.println(" reverse booking me hien");
-//         try{
-//              Booking booking=bookingRepository.findBookingById(bookingEvent.getId());
-//              List<ShowSeat> showSeatList=booking.getShowSeats();
-//              System.out.println("aa gye");
-//              for(ShowSeat showSeat:showSeatList){
-//                   showSeat.setShowSeatStatus(ShowSeatStatus.EMPTY);
-//                   showSeatRepository.save(showSeat);
-//              }
-//              System.out.println("empty kardi hein");
-//              booking.setBookingStatus(BookingStatus.FAILED);
-//              bookingRepository.save(booking);
-//         }
-//
-//         catch(Exception e){
-//              throw new RuntimeException("Seats cannot be emptied try again!!!");
-//          }
-//     }
-//
-//     @KafkaListener(topics="demo",groupId = "booking-group")
-//     public void Demo(String message){
-//          System.out.println(message);
-//          System.out.println("event aa gya");
-//     }
+     private final BookingRepository bookingRepository;
+     private final ShowSeatRepository showSeatRepository;
+     public ReverseBooking(BookingRepository bookingRepository, ShowSeatRepository showSeatRepository) {
+         this.bookingRepository = bookingRepository;
+         this.showSeatRepository = showSeatRepository;
+     }
+
+     @KafkaListener(topics="reverse-booking",groupId = "booking-group")
+     public void reverseBooking(BookingEvent bookingEvent){
+          System.out.println(" reverse booking me hien");
+         try{
+              Booking booking=bookingRepository.findBookingById(bookingEvent.getId());
+              List<ShowSeat> showSeatList=booking.getShowSeats();
+              System.out.println("aa gye");
+              for(ShowSeat showSeat:showSeatList){
+                   showSeat.setShowSeatStatus(ShowSeatStatus.EMPTY);
+                   showSeatRepository.save(showSeat);
+              }
+              System.out.println("empty kardi hein");
+              booking.setBookingStatus(BookingStatus.FAILED);
+              bookingRepository.save(booking);
+         }
+
+         catch(Exception e){
+              throw new RuntimeException("Seats cannot be emptied try again!!!");
+          }
+     }
+
+     @KafkaListener(topics="demo",groupId = "booking-group")
+     public void Demo(String message){
+          System.out.println(message);
+          System.out.println("event aa gya");
+     }
 }
---
+++ src/main/java/com/example/bookmyshow/Models/Grooks.java
@@ -0,0 +1,8 @@
+package com.example.bookmyshow.Models;
+
+public class Grooks {
+    int backend;
+    String founder;
+    String co-founder;
+    int age;
+}
